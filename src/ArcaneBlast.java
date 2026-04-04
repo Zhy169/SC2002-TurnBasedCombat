e20159ecadd2b3668e40fc940df114c2dcda17fa
Zhang Haoyuan <c250167@e.ntu.edu.sg>
2026-04-05 00:55:44 +0800

    Update ArcaneBlast.java

--- src/ArcaneBlast.java
+++ src/ArcaneBlast.java
@@ -1,28 +1,19 @@
 import java.util.List;
-
 public class ArcaneBlast implements Action {
+    @Override
     public void execute(Combatant performer, List<Combatant> targets) {
         int kills = 0;
         for (Combatant target : targets) {
             if (target.getHealth().isDead()) continue;
-
-            int atk = performer.getAttack().getValue();
-            int def = target.getTotalDefense();
-            int damage = Math.max(0, atk - def);
-
             int hpBefore = target.getHealth().getValue();
-            performer.basicAttack(target);
-
-            if (target.getHealth().isDead() && hpBefore > 0) {
-                kills++;
-            }
+            new DamageCalculator(performer, target).executeDamage();
+            if (target.getHealth().isDead() && hpBefore > 0) kills++;
         }
-
         if (kills > 0) {
-            // Duration -1 means it never expires (until end of game)
             performer.addEffect(new AttackBuffEffect(kills * 10, -1));
-            System.out.println(performer.getName() + " absorbed souls! Attack + " + (kills * 10));
+            System.out.println(performer.getName() + " gains " + (kills * 10) + " attack.");
         }
     }
+    @Override
     public String getName() { return "Arcane Blast"; }
 }
